e58ca73dc92c5a6ab436c6cc8b37ccfef000b248
huangjunjie3 <huangjunjie3@jd.com>
2019-07-16 13:51:10 +0800

    add JSPServletJavaBean_Demo1

---
+++ JSPServletJavaBean_Demo1/src/util/DBHelper.java
@@ -0,0 +1,73 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+
+public class DBHelper {
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private static final String url = "jdbc:mysql://localhost:3306/shopping?useUnicode=true&characterEncoding=UTF-8";
+    private static final String username = "root";
+    private static final String password = "5211314";
+
+    private static Connection conn = null;
+
+    static {
+        try {
+            Class.forName(driver);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws Exception
+    {
+        if (conn == null){
+            conn = DriverManager.getConnection(url,username,password);
+            return conn;
+        }
+        return conn;
+    }
+    public static void main(String[] args)
+    {
+        try {
+            Connection conn = DBHelper.getConnection();
+            if(conn != null)
+            {
+                System.out.println("数据库连接正常！");
+            }
+            else
+            {
+                System.out.println("数据库连接异常！");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
